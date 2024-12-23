4dbcd2abca754a94fcbeb79e360222587b5a94fb
getm0ss1moving <JDmoss1@sjtu.edu.cn>
2024-12-23 21:40:21 +0800

    更改了runtimeerror的形式

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,8 @@ void REPL()
         }
         catch (const RuntimeError &RE)
         {
-            std :: cout << RE.message();
-            // std :: cout << "RuntimeError";
+            // std :: cout << RE.message();
+            std :: cout << "RuntimeError";
         }
         puts("");
     }
